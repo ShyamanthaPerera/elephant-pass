39e67621d0a7804fefecb78255a364e821650ea0
ShyamanthaPerera <shyamanthaperera64@gmail.com>
2023-09-08 00:20:00 +0530

    Create new fxml files and classes.

--- src/main/java/com/example/elephantpass/CreateAccController.java
+++ src/main/java/com/example/elephantpass/CreateAccController.java
@@ -3,19 +3,44 @@ package com.example.elephantpass;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CreateAccController {
 
 
+    public JFXTextField txtLasName;
+    public JFXTextField txtETF;
+    public JFXTextField txtNIC;
+    public JFXTextField txtUsername;
+    public JFXTextField txtPassword;
     @FXML
     private JFXTextField txtFirstName;
 
     @FXML
-    void btnAction(ActionEvent event) {
+    void btnCreateAccAction(ActionEvent event) {
         System.out.println(txtFirstName.getText());
 
+//        try {
+//            AnchorPane parentContent = FXMLLoader.load(getClass().getResource(("src/main/resources/com/example/elephantpass/LoginForm.fxml")));
+//
+//            AnchorPane root = new AnchorPane();
+//            root.getChildren().clear();
+//            root.getChildren().setAll(parentContent);
+//
+//            ((Stage) root.getScene().getWindow()).setHeight(690);
+//            ((Stage) root.getScene().getWindow()).setWidth(1213);
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 
 
+    public void btnSignInAction(ActionEvent actionEvent) {
+    }
 }
--- src/main/java/com/example/elephantpass/LoginFormController.java
+++ src/main/java/com/example/elephantpass/LoginFormController.java
@@ -3,10 +3,23 @@ package com.example.elephantpass;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.security.cert.PolicyNode;
 
 public class LoginFormController {
-    public JFXButton btnAddElephant;
 
-    public void btnAddElephantAction(ActionEvent actionEvent) {
+    public JFXTextField txtUsername;
+    public JFXTextField txtPassword;
+    public JFXButton btnCreateAcc;
+    public JFXButton btnSignIn;
+
+    public void btnCreateAccAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSignInAction(ActionEvent actionEvent) {
     }
 }
