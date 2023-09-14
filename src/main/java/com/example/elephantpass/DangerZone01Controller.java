445e486097853f54eda005745550c051b910d656
ShyamanthaPerera <shyamanthaperera64@gmail.com>
2023-09-14 10:46:05 +0530

    Create some UI s and calsses.

---
+++ src/main/java/com/example/elephantpass/DangerZone01Controller.java
@@ -0,0 +1,51 @@
+package com.example.elephantpass;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class DangerZone01Controller {
+    public JFXButton btnDashboard;
+    public JFXButton btnLiveCamera;
+    public JFXButton btnSummaryReport;
+    public JFXButton btnElephantDetails;
+    public JFXButton btnVillagersDetails;
+    public JFXButton btnDangerZones;
+    public JFXButton btnComplains;
+    public JFXButton btnMap;
+    public JFXButton btnLogOut;
+    public Label lblDate;
+    public Label lblTime;
+    public Label dangerZone01ID;
+    public Label dangerZone01Name;
+    public Label dangerZone01Location;
+    public JFXTextArea dangerZone01Description;
+
+    public void btnDashboardAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLiveCameraAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSummaryReportAction(ActionEvent actionEvent) {
+    }
+
+    public void btnElephantDetailsAction(ActionEvent actionEvent) {
+    }
+
+    public void btnVillagersDetailsAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDangerZonesAction(ActionEvent actionEvent) {
+    }
+
+    public void btnComplainsAction(ActionEvent actionEvent) {
+    }
+
+    public void btnMapAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLogOutAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/example/elephantpass/MapFormController.java
@@ -0,0 +1,68 @@
+package com.example.elephantpass;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class MapFormController {
+    public JFXButton btnDashboard;
+    public JFXButton btnLiveCamera;
+    public JFXButton btnSummaryReport;
+    public JFXButton btnElephantDetails;
+    public JFXButton btnVillagersDetails;
+    public JFXButton btnDangerZones;
+    public JFXButton btnComplains;
+    public JFXButton btnMap;
+    public JFXButton btnLogOut;
+    public Label lblDate;
+    public Label lblTime;
+    public ImageView dangerZone01;
+    public ImageView dangerZone04;
+    public ImageView dangerZone02;
+    public ImageView dangerZone03;
+    public ImageView dangerZone05;
+
+    public void btnDashboardAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLiveCameraAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSummaryReportAction(ActionEvent actionEvent) {
+    }
+
+    public void btnElephantDetailsAction(ActionEvent actionEvent) {
+    }
+
+    public void btnVillagersDetailsAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDangerZonesAction(ActionEvent actionEvent) {
+    }
+
+    public void btnComplainsAction(ActionEvent actionEvent) {
+    }
+
+    public void btnMapAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLogOutAction(ActionEvent actionEvent) {
+    }
+
+    public void dangerZone01Action(MouseEvent mouseEvent) {
+    }
+
+    public void dangerZone04Action(MouseEvent mouseEvent) {
+    }
+
+    public void dangerZone02Action(MouseEvent mouseEvent) {
+    }
+
+    public void dangerZone03Action(MouseEvent mouseEvent) {
+    }
+
+    public void dangerZone05Action(MouseEvent mouseEvent) {
+    }
+}
