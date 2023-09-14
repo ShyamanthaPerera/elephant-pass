101a64e7c53d161bfbd1bd98e7c9cb9efa093a10
ShyamanthaPerera <shyamanthaperera64@gmail.com>
2023-09-14 10:19:16 +0530

    New SOUT

--- src/main/java/com/example/elephantpass/AppInitializer.java
+++ src/main/java/com/example/elephantpass/AppInitializer.java
@@ -21,5 +21,6 @@ public class AppInitializer extends Application {
     public static void main(String[] args) {
         launch();
         System.out.println("Hello Elephant-Pass");
+        System.out.println("Hi");
     }
 }
