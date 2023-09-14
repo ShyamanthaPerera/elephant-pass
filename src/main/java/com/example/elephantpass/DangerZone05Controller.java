4a968d2df8e93ab0e6b778483fb95a57595867eb
ShyamanthaPerera <shyamanthaperera64@gmail.com>
2023-09-14 11:25:34 +0530

    Danger Zones UIs finished.

---
+++ src/main/java/com/example/elephantpass/DangerZone05Controller.java
@@ -0,0 +1,52 @@
+package com.example.elephantpass;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class DangerZone05Controller {
+
+    public JFXButton btnDashboard;
+    public JFXButton btnLiveCamera;
+    public JFXButton btnSummaryReport;
+    public JFXButton btnElephantDetails;
+    public JFXButton btnVillagersDetails;
+    public JFXButton btnDangerZones;
+    public JFXButton btnComplains;
+    public JFXButton btnMap;
+    public JFXButton btnLogOut;
+    public Label lblDate;
+    public Label lblTime;
+    public Label dangerZone05ID;
+    public Label dangerZone05Name;
+    public Label dangerZone05Location;
+    public JFXTextArea dangerZone05Description;
+
+    public void btnDashboardAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLiveCameraAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSummaryReportAction(ActionEvent actionEvent) {
+    }
+
+    public void btnElephantDetailsAction(ActionEvent actionEvent) {
+    }
+
+    public void btnVillagersDetailsAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDangerZonesAction(ActionEvent actionEvent) {
+    }
+
+    public void btnComplainsAction(ActionEvent actionEvent) {
+    }
+
+    public void btnMapAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLogOutAction(ActionEvent actionEvent) {
+    }
+}
