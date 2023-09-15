60d3635a6d26e3794f57a92d557d75554ece3410
ShyamanthaPerera <shyamanthaperera64@gmail.com>
2023-09-15 12:27:08 +0530

    Create some UIs and Controller classes.

---
+++ src/main/java/com/example/elephantpass/ComplainsFormController.java
@@ -0,0 +1,65 @@
+package com.example.elephantpass;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+
+public class ComplainsFormController {
+    public JFXButton btnDashboard;
+    public JFXButton btnLiveCamera;
+    public JFXButton btnSummaryReport;
+    public JFXButton btnElephantDetails;
+    public JFXButton btnVillagersDetails;
+    public JFXButton btnDangerZones;
+    public JFXButton btnComplains;
+    public JFXButton btnMap;
+    public JFXButton btnLogOut;
+    public Label lblDate;
+    public Label lblTime;
+    public JFXTextField textVillagerID;
+    public JFXTextField textVillagerName;
+    public JFXTextField textComplainLocation;
+    public JFXTextField textComplainDescription;
+    public TableColumn villagerIDtbl;
+    public TableColumn villagerNameTbl;
+    public TableColumn complainLocationTbl;
+    public TableColumn complainDescriptionTbl;
+
+    public void btnDashboardAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLiveCameraAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSummaryReportAction(ActionEvent actionEvent) {
+    }
+
+    public void btnElephantDetailsAction(ActionEvent actionEvent) {
+    }
+
+    public void btnVillagersDetailsAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDangerZonesAction(ActionEvent actionEvent) {
+    }
+
+    public void btnComplainsAction(ActionEvent actionEvent) {
+    }
+
+    public void btnMapAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLogOutAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAddComplainAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateComplainAction(ActionEvent actionEvent) {
+    }
+
+    public void btnRemoveComplainAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/example/elephantpass/LiveCameraFormController.java
@@ -0,0 +1,47 @@
+package com.example.elephantpass;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class LiveCameraFormController {
+
+    public JFXButton btnDashboard;
+    public JFXButton btnLiveCamera;
+    public JFXButton btnSummaryReport;
+    public JFXButton btnElephantDetails;
+    public JFXButton btnVillagersDetails;
+    public JFXButton btnDangerZones;
+    public JFXButton btnComplains;
+    public JFXButton btnMap;
+    public JFXButton btnLogOut;
+    public Label lblDate;
+    public Label lblTime;
+
+    public void btnDashboardAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLiveCameraAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSummaryReportAction(ActionEvent actionEvent) {
+    }
+
+    public void btnElephantDetailsAction(ActionEvent actionEvent) {
+    }
+
+    public void btnVillagersDetailsAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDangerZonesAction(ActionEvent actionEvent) {
+    }
+
+    public void btnComplainsAction(ActionEvent actionEvent) {
+    }
+
+    public void btnMapAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLogOutAction(ActionEvent actionEvent) {
+    }
+}
